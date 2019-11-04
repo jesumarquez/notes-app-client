463f93582d5fee5f2d9dcbc679bc71770832d513
Jesú Márquez <jesumarquez@gmail.com>
2019-11-04 16:52:51 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { LinkContainer } from "react-router-bootstrap";
+import { Link } from "react-router-dom";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
+import "./App.css";
+import Routes from "./Routes";
+
+function App(props) {
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+  
+  function handleLogout() {
+    userHasAuthenticated(false);
+  }
+
+  return (
+    <div className="App container">
+      <Navbar fluid collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/">Scratch</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            {isAuthenticated
+              ? <NavItem onClick={handleLogout}>Logout</NavItem>
+              : <>
+                  <LinkContainer to="/signup">
+                    <NavItem>Signup</NavItem>
+                  </LinkContainer>
+                  <LinkContainer to="/login">
+                    <NavItem>Login</NavItem>
+                  </LinkContainer>
+                </>
+            }
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Routes appProps={{ isAuthenticated, userHasAuthenticated }} />
+    </div>
+  );
+}
+
+export default App;
